6f64d72fb3bd18dc184b45ebb927c1a53b5f1712
NQH1611 <88624093+NQH1611@users.noreply.github.com>
2022-11-14 12:29:38 +0700

    create package project

---
+++ banlaptop/src/main/java/com/shopbanlaptop/banlaptop/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.shopbanlaptop.banlaptop;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BanlaptopApplication.class);
+	}
+
+}
---
+++ banlaptop/src/test/java/com/shopbanlaptop/banlaptop/BanlaptopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopbanlaptop.banlaptop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanlaptopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
