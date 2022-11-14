be4c6cfd0af9a295cd48f71be2521dc7a9dc46c6
NQH1611 <88624093+NQH1611@users.noreply.github.com>
2022-11-14 14:28:29 +0700

    fix

--- banlaptop/src/main/java/com/shopbanlaptop/banlaptop/repository/ICustomerRepository.java
+++ banlaptop/src/main/java/com/shopbanlaptop/banlaptop/repository/ICustomerRepository.java
@@ -3,7 +3,7 @@ package com.shopbanlaptop.banlaptop.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.shopbanlaptop.banlaptop.model.Customers;
-
+//có sự thay đổi ở đây
 public interface ICustomerRepository extends JpaRepository<Customers, Integer>{
     
 }
